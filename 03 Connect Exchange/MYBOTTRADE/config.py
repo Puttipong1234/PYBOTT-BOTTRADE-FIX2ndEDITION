8fe019680afa97e1becd97880310e47b8b0f6db4
PYBOTT <pyybott@gmail.com>
2021-12-20 04:07:00 +0700

    add

--- 03 Connect Exchange/MYBOTTRADE/config.py
+++ 03 Connect Exchange/MYBOTTRADE/config.py
@@ -1,4 +1,4 @@
-BINANCE_API_KEY = "lWVXvvgTXxeYDZtemDs4qXKn74SyvXtSWGUGMd6UQtIq8jr0W3BrqRhboLeBtZY8 "
-BINANCE_API_SECRET = "sUPDG2ZabB3rcZ6PbONEZznBGJtbF1Nmy9yBJQSyH7yz8TAVIj4JmZ9kMgjYrbIx"
+BINANCE_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+BINANCE_API_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
-LINE_NOTIFY_API = "bYMefbv4lFK3Bn5esd45e8SqVmw78oHsqL9LrIVQ2DZ"
+LINE_NOTIFY_API = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
