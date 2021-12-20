2e3c2448de4d7269432f94f19be0372fd26e9a19
PYBOTT <pyybott@gmail.com>
2021-12-20 20:27:44 +0700

    add

--- 03 Connect Exchange/MYBOTTRADE/config.py
+++ 03 Connect Exchange/MYBOTTRADE/config.py
@@ -1,4 +1,18 @@
-BINANCE_API_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-BINANCE_API_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+import os
 
-LINE_NOTIFY_API = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+# BINANCE_API_KEY = "xxxxxxxxxxxxxxxx"
+# BINANCE_API_SECRET = "xxxxxxxxxxxxxxxx"
+
+# LINE_NOTIFY_API = "xxxxxxxxxxxxxxxx"
+
+# แก้ไขเวลา Deploy ขึ้น heroku ใช้ ENV variable เพื่อปกป้อง api key
+# heroku config:set BINANCE_API_KEY=xxx
+# heroku config:set BINANCE_API_SECRET=xxx
+# heroku config:set LINE_NOTIFY_API=xxx
+# git add .
+# git commit -m "add"
+# git push heroku master
+
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
+LINE_NOTIFY_API = os.getenv("LINE_NOTIFY_API")
